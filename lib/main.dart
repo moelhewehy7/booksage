6ef32e2f1bb809fc8a49850f8ea3239e6a6ce0de
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-01-07 00:56:51 +0200

    add googlefont

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'features/splash/presentation/views/onboardingview.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const Reading());
 }
 
 class Reading extends StatelessWidget {
-  const Reading({super.key});
+  const Reading({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
+      theme:
+          ThemeData.light().copyWith(textTheme: GoogleFonts.robotoTextTheme()),
       // ignore: prefer_const_constructors
       home: OnBoardingView(),
     );
