18e8eab4cce42337d7dbfbda8e6ee6043c6935f7
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-01-06 22:04:17 +0200

    creating customeitem for listview

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:reading/core/utils/assets.dart';
+import 'package:reading/features/home/presentation/views/widgets/customelistviewitem.dart';
 
 import 'customappbar.dart';
 
@@ -9,12 +8,15 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Column(
       children: [
         const SizedBox(
           height: 20,
         ),
-        CustomAppBar()
+        CustomAppBar(),
+        CustomeListViewItem(width: width, height: height)
       ],
     );
   }
