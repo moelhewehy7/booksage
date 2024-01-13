320837af0ae2f2c32b9dc6b65ecbd4cc7f7d861c
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-01-13 21:30:19 +0200

    create FeaturedBooks method

--- lib/features/home/presentation/manager/books cubit/books_cubit.dart
+++ lib/features/home/presentation/manager/books cubit/books_cubit.dart
@@ -1,9 +1,18 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:reading/features/home/data/models/book_model/book_model.dart';
+import 'package:reading/features/home/data/repos/home_repo.dart';
 
 part 'books_state.dart';
 
 class BooksCubit extends Cubit<BooksState> {
-  BooksCubit() : super(BooksInitial());
+  BooksCubit(this.homerepo) : super(BooksInitial());
+  final HomeRepo homerepo;
+  Future<void> fetchBooks() async {
+    emit(BooksLoading());
+    var result = await homerepo.fetchFeaturedBooks();
+    result.fold(
+        (failure) => emit(BooksFailure(errMessage: failure.errorMessage)),
+        (books) => emit(BooksSuccess(books: books)));
+  }
 }
