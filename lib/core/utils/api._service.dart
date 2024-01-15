b4dc8f2d79747f9975dc7ef6044c8d8490be349f
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-01-15 21:42:01 +0200

    refactor

--- lib/core/utils/api._service.dart
+++ lib/core/utils/api._service.dart
@@ -2,10 +2,10 @@ import 'package:dio/dio.dart';
 
 class ApiService {
   final Dio _dio = Dio();
-  final baseurl = "https://www.googleapis.com/books/v1/";
+  final _baseurl = "https://www.googleapis.com/books/v1/";
 
   Future<Map<String, dynamic>> get({required String endpoint}) async {
-    Response response = await _dio.get("$baseurl$endpoint");
+    Response response = await _dio.get("$_baseurl$endpoint");
     return response.data;
   }
 }
--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -7,3 +7,6 @@ abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchNewstBooks();
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
+// The Repository Pattern in Flutter is a software design pattern 
+// that abstracts the logic that retrieves data
+
