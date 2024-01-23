5c409305587d894b41e263cf8ce47ad90001a58a
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-01-23 19:32:02 +0200

    HomeRemoteDataSource and HomeRemoteDataSourceImpl

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+import 'package:reading/features/home/data/models/book_model/book_model.dart';
+import '../../../../core/utils/api._service.dart';
 import '../../domain/entities/book_entity.dart';
 
 abstract class HomeRemoteDataSource {
@@ -6,15 +9,30 @@ abstract class HomeRemoteDataSource {
 }
 
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImpl(this.apiService);
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiService.get(
+        endpoint: "volumes?Filtering=free-ebooks&Sorting=newest&q=New");
+    List<dynamic> bookslist = data["items"];
+    List<BookEntity> books = [];
+    for (var item in bookslist) {
+      books.add(BookModel.fromJson(item));
+    }
+    return books;
   }
 
   @override
-  Future<List<BookEntity>> fetchNewstBooks() {
-    // TODO: implement fetchNewstBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchNewstBooks() async {
+    var data = await apiService.get(
+        endpoint: 'volumes?Filtering=free-ebooks&q=book:sport');
+    List<dynamic> bookslist = data["items"];
+    List<BookEntity> books = [];
+    for (var item in bookslist) {
+      books.add(BookModel.fromJson(item));
+    }
+    return books;
   }
 }
