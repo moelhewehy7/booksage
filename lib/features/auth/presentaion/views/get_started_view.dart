2380c0a0685c373c403a7d976f75246dc1e6ce63
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2024-03-18 21:27:28 +0200

    blocks the current route from being popped

--- lib/features/auth/presentaion/views/get_started_view.dart
+++ lib/features/auth/presentaion/views/get_started_view.dart
@@ -19,115 +19,118 @@ class _GetStartedViewState extends State<GetStartedView> {
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.sizeOf(context).height;
-    return Scaffold(
-      body: SafeArea(
-        minimum: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Spacer(),
-            FadeInSlide(
-              duration: .4,
-              child: SizedBox(
-                  height: height * 0.15, child: Image.asset(AssetsData.logo)),
-            ),
-            const Spacer(),
-            FadeInSlide(
-              duration: .5,
-              child: Text(
-                "Let's Get Started!",
-                style: Styles.textStyle20.copyWith(color: kprimarycolor),
-                textAlign: TextAlign.center,
+    return PopScope(
+      canPop: false, //When false, blocks the current route from being popped.
+      child: Scaffold(
+        body: SafeArea(
+          minimum: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Spacer(),
+              FadeInSlide(
+                duration: .4,
+                child: SizedBox(
+                    height: height * 0.15, child: Image.asset(AssetsData.logo)),
               ),
-            ),
-            SizedBox(height: height * 0.015),
-            FadeInSlide(
-              duration: .6,
-              child: Text(
-                "Let's dive into some great books.",
-                style: Styles.textStyle18.copyWith(color: kprimarycolor),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const Spacer(),
-            FadeInSlide(
-              duration: .7,
-              child: LoginButton(
-                icon: Brand(Brands.google, size: 25),
-                text: "Continue with Google",
-                onPressed: () {},
-              ),
-            ),
-            SizedBox(height: height * 0.02),
-            FadeInSlide(
-              duration: .9,
-              child: LoginButton(
-                icon: Brand(Brands.facebook, size: 25),
-                text: "Continue with Facebook",
-                onPressed: () {},
-              ),
-            ),
-            SizedBox(height: height * 0.02),
-            FadeInSlide(
-              duration: 1.0,
-              child: LoginButton(
-                icon: Brand(Brands.twitterx, size: 25),
-                text: "Continue with X",
-                onPressed: () {},
-              ),
-            ),
-            const Spacer(),
-            FadeInSlide(
-              duration: 1.1,
-              child: FilledButton(
-                onPressed: () {
-                  GoRouter.of(context).push(AppRouter.kSignup);
-                },
-                style: FilledButton.styleFrom(
-                  backgroundColor: const Color(0xFF5F433A),
-                  fixedSize: const Size.fromHeight(50),
-                ),
-                child: const Text(
-                  "Sign Up",
-                  style: TextStyle(fontWeight: FontWeight.w500),
+              const Spacer(),
+              FadeInSlide(
+                duration: .5,
+                child: Text(
+                  "Let's Get Started!",
+                  style: Styles.textStyle20.copyWith(color: kprimarycolor),
+                  textAlign: TextAlign.center,
                 ),
               ),
-            ),
-            SizedBox(height: height * 0.02),
-            FadeInSlide(
-              duration: 1.2,
-              child: FilledButton(
-                onPressed: () {
-                  GoRouter.of(context).push(AppRouter.kSignin);
-                },
-                style: FilledButton.styleFrom(
-                  backgroundColor: const Color(0xFFFFDCD1),
-                  fixedSize: const Size.fromHeight(50),
-                ),
-                child: const Text(
-                  "Sign In",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500, color: kprimarycolor),
+              SizedBox(height: height * 0.015),
+              FadeInSlide(
+                duration: .6,
+                child: Text(
+                  "Let's dive into some great books.",
+                  style: Styles.textStyle18.copyWith(color: kprimarycolor),
+                  textAlign: TextAlign.center,
                 ),
               ),
-            ),
-            const Spacer(),
-            const FadeInSlide(
-              duration: 1.0,
-              direction: FadeSlideDirection.btt,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Privacy Policy"),
-                  Text("   -   "),
-                  Text("Terms of Service"),
-                ],
+              const Spacer(),
+              FadeInSlide(
+                duration: .7,
+                child: LoginButton(
+                  icon: Brand(Brands.google, size: 25),
+                  text: "Continue with Google",
+                  onPressed: () {},
+                ),
               ),
-            ),
-            SizedBox(height: height * 0.02),
-          ],
+              SizedBox(height: height * 0.02),
+              FadeInSlide(
+                duration: .9,
+                child: LoginButton(
+                  icon: Brand(Brands.facebook, size: 25),
+                  text: "Continue with Facebook",
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(height: height * 0.02),
+              FadeInSlide(
+                duration: 1.0,
+                child: LoginButton(
+                  icon: Brand(Brands.twitterx, size: 25),
+                  text: "Continue with X",
+                  onPressed: () {},
+                ),
+              ),
+              const Spacer(),
+              FadeInSlide(
+                duration: 1.1,
+                child: FilledButton(
+                  onPressed: () {
+                    GoRouter.of(context).push(AppRouter.kSignup);
+                  },
+                  style: FilledButton.styleFrom(
+                    backgroundColor: const Color(0xFF5F433A),
+                    fixedSize: const Size.fromHeight(50),
+                  ),
+                  child: const Text(
+                    "Sign Up",
+                    style: TextStyle(fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+              SizedBox(height: height * 0.02),
+              FadeInSlide(
+                duration: 1.2,
+                child: FilledButton(
+                  onPressed: () {
+                    GoRouter.of(context).push(AppRouter.kSignin);
+                  },
+                  style: FilledButton.styleFrom(
+                    backgroundColor: const Color(0xFFFFDCD1),
+                    fixedSize: const Size.fromHeight(50),
+                  ),
+                  child: const Text(
+                    "Sign In",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500, color: kprimarycolor),
+                  ),
+                ),
+              ),
+              const Spacer(),
+              const FadeInSlide(
+                duration: 1.0,
+                direction: FadeSlideDirection.btt,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Privacy Policy"),
+                    Text("   -   "),
+                    Text("Terms of Service"),
+                  ],
+                ),
+              ),
+              SizedBox(height: height * 0.02),
+            ],
+          ),
         ),
       ),
     );
